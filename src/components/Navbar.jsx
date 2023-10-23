2a069917a6c6bd2b72f40ad63ee528dabc53ad90
Даурен Бекузаков <daurenswrld@MacBook-Air-Dauren.local>
2023-10-23 21:24:49 +0600

    Added some features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,24 @@
-import React, {useEffect,useState} from 'react'
-import { Button, Menu, Typography,Avatar } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
-import { HomeOutlined,MoneyCollectOutlined,BulbOutlined,FundOutlined,MenuOutlined } from '@ant-design/icons'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
 
 import icon from '../images/cryptocurrency.png'
 
 function Navbar() {
 
-  const [activeMenu,setActiveMenu] = useState(true)
-  const [screenSize,setScreenSize ] =useState(null)
+  const [activeMenu, setActiveMenu] = useState(true)
+  const [screenSize, setScreenSize] = useState(null)
 
-  useEffect(() =>{
+  useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
 
-    window.addEventListener('resize' ,handleResize);
+    window.addEventListener('resize', handleResize);
     handleResize();
-    return() =>window.removeEventListener('resize' ,handleResize)
-  }, []) 
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
- 
+
   useEffect(() => {
     if (screenSize <= 800) {
       setActiveMenu(false);
@@ -29,34 +29,34 @@ function Navbar() {
   return (
     <div className="nav-container">
 
-        <div className="logo-container">
+      <div className="logo-container">
 
-            <Avatar src={icon} size='large'  />
-            <Typography.Title level={2} className='logo'>
+        <Avatar src={icon} size='large' />
+        <Typography.Title level={2} className='logo' style={{ marginBottom: 0 }}>
 
-                <Link to='/'>Cryptoverse</Link>
+          <Link to='/'>CryptoHorizon</Link>
 
 
-            </Typography.Title>
-            <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
-        </div>
-        {activeMenu &&(
+        </Typography.Title>
+        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
+      </div>
+      {activeMenu && (
         <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>  
-        )}
- 
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
+
     </div>
   )
 }
